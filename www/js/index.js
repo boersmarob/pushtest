daf1c34bf12ed5e1f45e4abceba68d6a799d35a9
RobLinux <info@robboersma.nl>
2015-05-28 19:49:27 +0200

    test

--- www/js/index.js
+++ www/js/index.js
@@ -39,6 +39,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+	alert("hoi");
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -82,4 +83,4 @@ var app = {
     }
 };
 
-//app.initialize();
+app.initialize();
